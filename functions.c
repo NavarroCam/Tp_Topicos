a825f2affaf22cdf73e4e02425ac7f0081d9b374
NavarroCam <cnavarroojeda@alumno.unlam.edu.ar>
2025-09-17 20:00:20 -0300

    Update functions.c

--- functions.c
+++ functions.c
@@ -2,3 +2,50 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
+
+typedef struct {
+    int dia;
+    int mes;
+    int anio;
+} Fecha;
+
+void ValidarFecha(Fecha f) 
+{
+    int diasEnMes;
+
+    if (f.mes < 1 || f.mes > 12) // Validar mes
+    {
+        printf("Fecha invalida: mes incorrecto.\n");
+        return;
+    }
+
+    // Calcular días del mes
+    if (f.mes == 1 || f.mes == 3 || f.mes == 5 || f.mes == 7 || f.mes == 8 || f.mes == 10 || f.mes == 12)
+        diasEnMes = 31;
+    else 
+    {
+      if (f.mes == 4 || f.mes == 6 || f.mes == 9 || f.mes == 11)
+        diasEnMes = 30;
+      else // Febrero
+      {
+        if ((f.anio % 4 == 0 && f.anio % 100 != 0) || (f.anio % 400 == 0))
+            diasEnMes = 29;  // Año bisiesto
+        else
+            diasEnMes = 28;
+      }
+    }
+
+    if (f.dia < 1 || f.dia > diasEnMes) // Validar día
+    {
+        printf("Fecha invalida: dia incorrecto.\n");
+        return;
+    }
+
+    if (f.anio < 1) // Validar año
+    {
+        printf("Fecha invalida: año incorrecto.\n");
+        return;
+    }
+
+    printf("La fecha %02d/%02d/%04d es valida.\n", f.dia, f.mes, f.anio);
+}
