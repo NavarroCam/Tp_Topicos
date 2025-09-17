49ba3fd3ee11f4866f77aad19b6b55d29d51cbbf
NavarroCam <cnavarroojeda@alumno.unlam.edu.ar>
2025-09-17 20:46:51 -0300

    Update functions.h

--- functions.h
+++ functions.h
@@ -1,5 +1,9 @@
 #ifndef FUNCTIONS_H_INCLUDED
 #define FUNCTIONS_H_INCLUDED
 
+#include <time.h>
+
+void validarFecha(t_fecha f);
+bool ValidarFechaNacimiento(t_fecha nacimiento); 
 
 #endif // FUNCTIONS_H_INCLUDED
