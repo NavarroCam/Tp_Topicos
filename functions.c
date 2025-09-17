72616c934fa6d4d56a8c3c790da0c5056bc4afe7
NavarroCam <cnavarroojeda@alumno.unlam.edu.ar>
2025-09-17 20:44:53 -0300

    Update functions.c

--- functions.c
+++ functions.c
@@ -14,7 +14,7 @@ void validarFecha(t_fecha f)
         return;
     }
 
-    // Calcular días del mes
+    // Calcular dÃ­as del mes
     if (f.mes == 1  f.mes == 3  f.mes == 5  f.mes == 7  f.mes == 8  f.mes == 10  f.mes == 12)
         diasEnMes = 31;
     else
@@ -24,23 +24,50 @@ void validarFecha(t_fecha f)
       else // Febrero
       {
         if ((f.anio % 4 == 0 && f.anio % 100 != 0)  (f.anio % 400 == 0))
-            diasEnMes = 29;  // Año bisiesto
+            diasEnMes = 29;  // AÃ±o bisiesto
         else
             diasEnMes = 28;
       }
     }
 
-    if (f.dia < 1  f.dia > diasEnMes) // Validar día
+    if (f.dia < 1  f.dia > diasEnMes) // Validar dÃ­a
     {
         printf("Fecha invalida: dia incorrecto.\n");
         return;
     }
 
-    if (f.anio < 1) // Validar año
+    if (f.anio < 1) // Validar aÃ±o
     {
-        printf("Fecha invalida: año incorrecto.\n");
+        printf("Fecha invalida: aÃ±o incorrecto.\n");
         return;
     }
 
     printf("La fecha %02d/%02d/%04d es valida.\n", f.dia, f.mes, f.anio);
+}
+
+bool ValidarFechaNacimiento(t_fecha nacimiento) 
+{
+    time_t t;                 
+    struct tm *fecha;         
+
+    // Obtener tiempo actual
+    time(&t);
+
+    // Convertir a fecha local
+    fecha = localtime(&t);
+
+    // Fecha del sistema
+    int diaHoy  = fecha->tm_mday;
+    int mesHoy  = fecha->tm_mon + 1;     
+    int anioHoy = fecha->tm_year + 1900; 
+
+    printf("Hoy es: %02d/%02d/%d\n", diaHoy, mesHoy, anioHoy);
+
+    // Calcular edad
+    int edad = anioHoy - nacimiento.anio;
+
+    if (mesHoy < nacimiento.mes || (mesHoy == nacimiento.mes && diaHoy < nacimiento.dia))
+        edad--;
+
+    return edad >= 10;
 }
