46b010e2e6d1046d6c9a98b06b575bd0ab6cf877
NavarroCam <cnavarroojeda@alumno.unlam.edu.ar>
2025-09-17 20:48:00 -0300

    Update functions.c

--- functions.c
+++ functions.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
-
+#include <time.h>
 
 void validarFecha(t_fecha f)
 {
@@ -14,7 +14,7 @@ void validarFecha(t_fecha f)
         return;
     }
 
-    // Calcular días del mes
+    // Calcular dÃ­as del mes
     if (f.mes == 1  f.mes == 3  f.mes == 5  f.mes == 7  f.mes == 8  f.mes == 10  f.mes == 12)
         diasEnMes = 31;
     else
@@ -24,21 +24,21 @@ void validarFecha(t_fecha f)
       else // Febrero
       {
         if ((f.anio % 4 == 0 && f.anio % 100 != 0)  (f.anio % 400 == 0))
-            diasEnMes = 29;  // Año bisiesto
+            diasEnMes = 29;  // AÃ±o bisiesto
         else
             diasEnMes = 28;
       }
     }
 
-    if (f.dia < 1  f.dia > diasEnMes) // Validar día
+    if (f.dia < 1  f.dia > diasEnMes) // Validar dÃ­a
     {
         printf("Fecha invalida: dia incorrecto.\n");
         return;
     }
 
-    if (f.anio < 1) // Validar año
+    if (f.anio < 1) // Validar aÃ±o
     {
-        printf("Fecha invalida: año incorrecto.\n");
+        printf("Fecha invalida: aÃ±o incorrecto.\n");
         return;
     }
 
